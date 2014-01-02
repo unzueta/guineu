f7de18ffd0468acdc333bacb23095ab40e2aafba
Christof Wollenhaupt <christof.wollenhaupt@foxpert.com>
2014-01-02 11:46:06 +0100

    Fixed #1 property value not found

--- Source/Guineu.Runtime.Desktop/Gui/Desktop/DesktopEditbox.cs
+++ Source/Guineu.Runtime.Desktop/Gui/Desktop/DesktopEditbox.cs
@@ -5,174 +5,215 @@ using Guineu.Expression;
 
 namespace Guineu.Gui.Desktop
 {
-    class DesktopEditbox : TextBox, IControl
-    {
-        public DesktopEditbox()
-        {
-            ScrollBars = ScrollBars.Vertical;
-        }
+	sealed class DesktopEditbox : TextBox, IControl
+	{
+		public DesktopEditbox()
+		{
+			Multiline = true;
+			ScrollBars = ScrollBars.Vertical;
+		}
 
-        protected override void OnValidating(CancelEventArgs e)
-        {
-            base.OnValidating(e);
-            this.ValidEvent(this, EventHandler, ValueHandling.TextToValue(Text, VariantType.Character));
-        }
-        protected override void OnEnter(EventArgs e)
-        {
-            base.OnEnter(e);
-            this.WhenEvent(this, EventHandler);
-        }
-        protected override void OnClick(EventArgs e)
-        {
-            base.OnClick(e);
-            this.CallEvent(EventHandler, KnownNti.Click);
-        }
-        protected override void OnGotFocus(EventArgs e)
-        {
-            base.OnGotFocus(e);
-            this.CallEvent(EventHandler, KnownNti.GotFocus);
-        }
-        protected override void OnLostFocus(EventArgs e)
-        {
-            base.OnLostFocus(e);
-            this.CallEvent(EventHandler, KnownNti.LostFocus);
-        }
+		protected override void OnValidating(CancelEventArgs e)
+		{
+			base.OnValidating(e);
+			this.ValidEvent(this, EventHandler, ValueHandling.TextToValue(Text, VariantType.Character));
+		}
+		protected override void OnEnter(EventArgs e)
+		{
+			base.OnEnter(e);
+			this.WhenEvent(this, EventHandler);
+		}
+		protected override void OnClick(EventArgs e)
+		{
+			base.OnClick(e);
+			this.CallEvent(EventHandler, KnownNti.Click);
+		}
+		protected override void OnGotFocus(EventArgs e)
+		{
+			base.OnGotFocus(e);
+			this.GotFocusEvent(this, EventHandler);
+		}
+		protected override void OnLostFocus(EventArgs e)
+		{
+			base.OnLostFocus(e);
+			this.LostFocusEvent(this, EventHandler, GetValue());
+		}
 
-        public void SetVariant(KnownNti nti, Variant value)
-        {
-            lock (this)
-            {
-                switch (nti)
-                {
-                    case KnownNti.BackColor:
-                        BackColor = new Color(value);
-                        break;
+		public void SetVariant(KnownNti nti, Variant value)
+		{
+			lock (this)
+			{
+				switch (nti)
+				{
+					case KnownNti.BackColor:
+						BackColor = new Color(value);
+						break;
 
-                    case KnownNti.Caption:
-                        Text = value.ToString();
-                        break;
+					case KnownNti.Enabled:
+						Enabled = value;
+						break;
 
-                    case KnownNti.Enabled:
-                        Enabled = value;
-                        break;
+					case KnownNti.ForeColor:
+						ForeColor = new Color(value);
+						break;
 
-                    case KnownNti.ForeColor:
-                        ForeColor = new Color(value);
-                        break;
+					case KnownNti.Left:
+						this.SetLeft(value);
+						break;
 
-                    case KnownNti.Left:
-                        this.SetLeft(value);
-                        break;
+					case KnownNti.ReadOnly:
+						ReadOnly = value;
+						break;
 
-                    case KnownNti.ReadOnly:
-                        ReadOnly = value;
-                        break;
+					case KnownNti.TabIndex:
+						TabIndex = value;
+						break;
 
-                    case KnownNti.TabIndex:
-                        TabIndex = value;
-                        break;
+					case KnownNti.Width:
+						this.SetWidth(value);
+						break;
 
-                    case KnownNti.Width:
-                        this.SetWidth(value);
-                        break;
+					case KnownNti.Top:
+						this.SetTop(value);
+						break;
 
-                    case KnownNti.Top:
-                        this.SetTop(value);
-                        break;
+					case KnownNti.Height:
+						this.SetHeight(value);
+						break;
 
-                    case KnownNti.Height:
-                        this.SetHeight(value);
-                        break;
+					case KnownNti.SelStart:
+						SelectionStart = value;
+						break;
 
-                    case KnownNti.SelStart:
-                        SelectionStart = value;
-                        break;
+					case KnownNti.SelLength:
+						SelectionLength = value;
+						break;
 
-                    case KnownNti.SelLength:
-                        SelectionLength = value;
-                        break;
+					case KnownNti.Value:
+						type = value.Type;
+						Text = value;
+						break;
 
-                    case KnownNti.Visible:
-                        Visible = value;
-                        break;
+					case KnownNti.Visible:
+						Visible = value;
+						break;
 
-                    default:
-                        if (FontHandling.Handles(nti))
-                        {
-                            FontHandling.Set(this, nti, value);
-                            break;
-                        }
+					default:
+						if (FontHandling.Handles(nti))
+						{
+							FontHandling.Set(this, nti, value);
+							break;
+						}
 
-                        throw new ErrorException(ErrorCodes.PropertyIsNotFound);
-                }
-            }
-        }
+						throw new ErrorException(ErrorCodes.PropertyIsNotFound, nti);
+				}
+			}
+		}
 
-        public Variant GetVariant(KnownNti nti)
-        {
-            switch (nti)
-            {
-                case KnownNti.BackColor:
-                    return new Variant((Int32)(Color)BackColor, 10);
+		public Variant GetVariant(KnownNti nti)
+		{
+			switch (nti)
+			{
+				case KnownNti.BackColor:
+					return new Variant((Int32)(Color)BackColor, 10);
 
-                case KnownNti.Enabled:
-                    return new Variant(Enabled);
+				case KnownNti.Enabled:
+					return new Variant(Enabled);
 
-                case KnownNti.ForeColor:
-                    return new Variant((Int32)(Color)ForeColor, 10);
+				case KnownNti.ForeColor:
+					return new Variant((Int32)(Color)ForeColor, 10);
 
-                case KnownNti.Left:
-                    return new Variant(Left, 10);
+				case KnownNti.Left:
+					return new Variant(Left, 10);
 
-                case KnownNti.ReadOnly:
-                    return new Variant(ReadOnly);
+				case KnownNti.ReadOnly:
+					return new Variant(ReadOnly);
 
-                case KnownNti.Top:
-                    return new Variant(Top, 10);
+				case KnownNti.Top:
+					return new Variant(Top, 10);
 
-                case KnownNti.Width:
-                    return new Variant(Width, 10);
+				case KnownNti.Width:
+					return new Variant(Width, 10);
 
-                case KnownNti.Height:
-                    return new Variant(Height, 10);
+				case KnownNti.Height:
+					return new Variant(Height, 10);
 
-                case KnownNti.SelStart:
-                    return new Variant(SelectionStart, 10);
+				case KnownNti.SelStart:
+					return new Variant(SelectionStart, 10);
 
-                case KnownNti.SelLength:
-                    return new Variant(SelectionLength, 10);
+				case KnownNti.SelLength:
+					return new Variant(SelectionLength, 10);
 
-                case KnownNti.TabIndex:
-                    return new Variant(TabIndex, 10);
+				case KnownNti.TabIndex:
+					return new Variant(TabIndex, 10);
 
-                case KnownNti.Visible:
-                    return new Variant(Visible);
+				case KnownNti.Value:
+					return GetValue();
 
-                default:
-                    if (FontHandling.Handles(nti))
-                        return FontHandling.Get(this, nti);
+				case KnownNti.Visible:
+					return new Variant(Visible);
 
-                    throw new ErrorException(ErrorCodes.PropertyIsNotFound);
-            }
-        }
+				default:
+					if (FontHandling.Handles(nti))
+						return FontHandling.Get(this, nti);
 
-        public Variant CallMethod(KnownNti name, ParameterCollection parms)
-        {
-            switch (name)
-            {
-                case KnownNti.SetFocus:
-                    Focus();
-                    return new Variant(true);
+					throw new ErrorException(ErrorCodes.PropertyIsNotFound);
+			}
+		}
 
-                case KnownNti.Move:
-                    this.MoveControl(parms);
-                    return new Variant(true);
+		VariantType type;
+		private Variant GetValue()
+		{
+			switch (type)
+			{
+				//case VariantType.Integer:
+				//  break;
+				//case VariantType.Logical:
+				//  break;
+				case VariantType.Character:
+					return new Variant(Text);
+				//case VariantType.Number:
+				//  break;
+				//case VariantType.Object:
+				//  break;
+				case VariantType.Date:
+					DateTime dt;
+					try
+					{
+						dt = DateTime.Parse(Text);
+					}
+					catch
+					{
+						dt = new DateTime(0);
+					}
+					return new Variant(dt);
+				//case VariantType.DateTime:
+				//  break;
+				//case VariantType.Null:
+				//  break;
+				//case VariantType.Unknown:
+				//  break;
+				default:
+					return new Variant(Text);
+			}
+		}
+		public Variant CallMethod(KnownNti name, ParameterCollection parms)
+		{
+			switch (name)
+			{
+				case KnownNti.SetFocus:
+					Focus();
+					return new Variant(true);
 
-                default:
-                    throw new ErrorException(ErrorCodes.PropertyIsNotFound);
-            }
-        }
+				case KnownNti.Move:
+					this.MoveControl(parms);
+					return new Variant(true);
 
-        public event Action<EventData> EventHandler;
-    }
+				default:
+					throw new ErrorException(ErrorCodes.PropertyIsNotFound);
+			}
+		}
+
+		public event Action<EventData> EventHandler;
+	}
 }
--- Source/Guineu.Runtime.Desktop/Gui/Desktop/FocusExtension.cs
+++ Source/Guineu.Runtime.Desktop/Gui/Desktop/FocusExtension.cs
@@ -4,28 +4,38 @@ using Guineu.Expression;
 
 namespace Guineu.Gui.Desktop
 {
-    static class FocusExtension
-    {
-        public static void WhenEvent(this Control obj, IControl ctrl, Action<EventData> proc)
-        {
-            if (!ctrl.CallEvent(proc, KnownNti.When))
-                obj.Parent.SelectNextControl(obj, true, true, true, true);
-            // TODO: Prevent eternal loop
-        }
+	static class FocusExtension
+	{
+		public static void WhenEvent(this Control obj, IControl ctrl, Action<EventData> proc)
+		{
+			if (!ctrl.CallEvent(proc, KnownNti.When))
+				obj.Parent.SelectNextControl(obj, true, true, true, true);
+			// TODO: Prevent eternal loop
+		}
 
-        public static Boolean ValidEvent(this Control obj, IControl ctrl, Action<EventData> proc, Variant newValue)
-        {
-            Boolean isValid = ctrl.CallEvent(proc, KnownNti.Valid);
-            if (isValid)
-                ctrl.RaiseEvent(proc, KnownNti.ControlSource,
-                                                new ParameterCollection(new[] { newValue })
-                    );
-            return isValid;
-        }
+		public static Boolean ValidEvent(this Control obj, IControl ctrl, Action<EventData> proc, Variant newValue)
+		{
+			Boolean isValid = ctrl.CallEvent(proc, KnownNti.Valid);
+			if (isValid)
+				ctrl.RaiseEvent(proc, KnownNti.ControlSource,
+																				new ParameterCollection(new[] { newValue })
+						);
+			return isValid;
+		}
 
-        public static Boolean ValidEvent(this Control obj, IControl ctrl, Action<EventData> onEventHandler)
-        {
-            return ctrl.CallEvent(onEventHandler, KnownNti.Valid);
-        }
-    }
+		public static Boolean ValidEvent(this Control obj, IControl ctrl, Action<EventData> onEventHandler)
+		{
+			return ctrl.CallEvent(onEventHandler, KnownNti.Valid);
+		}
+
+		public static void GotFocusEvent(this Control obj, IControl ctrl, Action<EventData> onEventHandler)
+		{
+			ctrl.CallEvent(onEventHandler, KnownNti.GotFocus);
+		}
+
+		public static void LostFocusEvent(this Control obj, IControl ctrl, Action<EventData> onEventHandler, Variant newValue)
+		{
+			ctrl.CallEvent(onEventHandler, KnownNti.LostFocus);
+		}
+	}
 }
